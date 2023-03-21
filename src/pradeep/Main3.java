20b98b3c36b1d1318ba581001d652991331624f3
yekol <yekol@LAPTOP-O0F0P2QQ>
2023-03-21 10:08:25 +0530

    Pushing to Changed1

---
+++ src/pradeep/Main3.java
@@ -0,0 +1,18 @@
+package pradeep;
+
+public class Main3 {
+	static int fun( int x, int y) {
+		if (x==0)
+		{
+			return y;
+			
+		}
+		return fun(x-1, x+y);
+		
+		
+	}
+	public static void main(String[] args) {
+		System.out.println(fun(4,3));
+	}
+
+}
